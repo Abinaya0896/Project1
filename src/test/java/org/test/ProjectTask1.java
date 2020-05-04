c0038e8864530221606cd41acff07589c642976f
Abinaya0896 <abiabinaya834@gmail.com>
2020-05-04 11:51:24 +0530

    Farith resolved conflicts and pushed the code to his branch

--- src/test/java/org/test/ProjectTask1.java
+++ src/test/java/org/test/ProjectTask1.java
@@ -3,7 +3,7 @@ package org.test;
 public class ProjectTask1 {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Hello");
 		System.out.println("Abi");
 		
 		System.out.println("Hello");
